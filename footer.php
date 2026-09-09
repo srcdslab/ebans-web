bc3a350fbe120ef9e9dd95732a8b8be9a46295c5
Rushaway <rushaway@hotmail.fr>
2026-09-09 07:18:29 +0200

    chore(deps): Update footer for ew4

--- footer.php
+++ footer.php
@@ -3,10 +3,10 @@
         <div class="flex flex-fd:column text:left">
             <span>Copyright © <a href="<?= e($GLOBALS['SERVER_FORUM_URL']) ?>" target="_blank" rel="noopener"><?= e($GLOBALS['SERVER_FORUM_NAME']) ?></a> 2016 - <?= date('Y') ?></span>
             <span><i class="fas fa-code"></i> Integration made by <a href="https://github.com/Dolly132" title="Integration made by Dolly" target="_blank" rel="noopener">Dolly</a></span>
-            <span>Co-Edited by <a href="https://github.com/Rushaway/" title="Rushaway's GitHub Profile" target="_blank" rel="noopener">.Rushaway</a></span>
+            <span>Co-Authored by <a href="https://github.com/Rushaway/" title="Rushaway's GitHub Profile" target="_blank" rel="noopener">.Rushaway</a></span>
         </div>
         <div class="flex flex-fd:column text:right">
-            <p>Powered by <a href="https://github.com/srcdslab/sm-plugin-EntWatch" title="EntWatch Repository" target="_blank" rel="noopener">EntWatch</a></p>
+            <p>Powered by <a href="https://github.com/srcdslab/sm-plugin-entwatch-4" title="EntWatch Repository" target="_blank" rel="noopener">EntWatch 4</a></p>
             <p>This Website is open source, check it out <a href="https://github.com/srcdslab/ebans-web" title="Ebans website Repository" target="_blank" rel="noopener">here</a></p>
         </div>
     </div>
